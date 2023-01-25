5d5fc7ef7d06c10d361316a8527a8e4e5446add2
Dima <futcorea@gmail.com>
2023-01-25 21:22:45 +0300

    What are you retirement age
    What are you retirement age

--- Main.java
+++ Main.java
@@ -1,20 +1,25 @@
 package com.age;
 
 public class Main {
+
     public static void main(String[] args) {
 
-      while (true){
-          Retirement_age in = new Retirement_age();
+        Retirement_age in = new Retirement_age();
+
+         while (true){
           int age = in.myAge();
           if(age >= 65){
-              System.out.println("You're lucky and can rest of your life happy!" + " Do you want to try again?");
+              System.out.println("You're lucky and can rest of your life happy!" + " Do you want to try again?\n");
           } else if (age > 0 && age <65) {
               int x = 65 - age;
-              System.out.println("You need to work " + x + " years!" + " Do you want to try again?");
+              System.out.println("You need to work " + x + " years!" + " Do you want to try again?\n");
+          } else if (age < 0) {
+              System.out.println("You've mistake!\n");
           } else if (age == 0)  {
-              System.out.println("See you soon! :-)");
-              break;
+             System.out.println("See you soon! :-)\n");
+             break;
           }
       }
-    }
+
+}
 }
--- Retirement_age.java
+++ Retirement_age.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class Retirement_age {
 
-    int myAge () {
+        int myAge () {
         System.out.println("What are you age?\n" + "If you want exit write - 0");
         Scanner in = new Scanner(System.in);
         int enterAge = in.nextInt();
         return enterAge;
     }
+
 }
